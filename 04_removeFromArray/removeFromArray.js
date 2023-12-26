ad77ae600610d21666a2a50df147377d6df8e723
Raina Patterson <hello.rainapatterson@gmail.com>
2023-12-26 11:18:12 -0600

    adding the parameters of the removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+function removeFromArray(passedArray, ...args) {
+    const newArray = []
+    passedArray.forEach(items => {
+       if (!args.includes(items)) {
+        newArray.push(items)
+       }
+    });
+    return newArray
 };
+
+// removeFromArray([1,2,3,4,],3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
